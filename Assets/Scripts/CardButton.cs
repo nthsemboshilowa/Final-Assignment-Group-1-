b570896f566f6d14b0107bd2ca2a2fae5c85d4d6
nthsemboshilowa <2464956@students.wits.ac.za>
2023-06-27 13:46:54 +0200

    final

--- Assets/Scripts/CardButton.cs
+++ Assets/Scripts/CardButton.cs
@@ -3,26 +3,6 @@ using UnityEngine.UI;
 
 public class CardButton : MonoBehaviour
 {
-    public DestinationCard destinationCard;
-    public Image originSprite;
-    public Image destinationSprite;
 
-    public Button button;
-
-    private void Start()
-    {
-        button = GetComponent<Button>();
-        button.onClick.AddListener(OnButtonClick);
-    }
-
-    private void OnButtonClick()
-    {
-        destinationCard.ToggleImage();
-    }
-
-    private void OnDestroy()
-    {
-        button.onClick.RemoveListener(OnButtonClick);
-    }
 }
 
--- Assets/Scripts/DestinationCard.cs
+++ Assets/Scripts/DestinationCard.cs
@@ -10,23 +10,9 @@ public class DestinationCard : MonoBehaviour
     public City Origin;
     public City Destination;
     public int PointValue;
-    public Image OriginImage;
-    public Image DestinationImage;
 
-    private bool isShowingOriginImage = true;
 
-    public void SetImages(Sprite originSprite, Sprite destinationSprite)
-    {
-        OriginImage.sprite = originSprite;
-        DestinationImage.sprite = destinationSprite;
-    }
 
-    public void ToggleImage()
-    {
-        isShowingOriginImage = !isShowingOriginImage;
-        OriginImage.gameObject.SetActive(isShowingOriginImage);
-        DestinationImage.gameObject.SetActive(!isShowingOriginImage);
-    }
     public DestinationCard(City origin, City destination, int points)
     {
         Origin = origin;
